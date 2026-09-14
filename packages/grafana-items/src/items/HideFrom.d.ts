2672a115d77fe5d400679416c8a5d1d4440a349d
gissehel <public-dev-git-capella@gissehel.org>
2026-09-14 09:17:02 +0200

    Release 0.1.16

--- packages/grafana-items/src/items/HideFrom.d.ts
+++ packages/grafana-items/src/items/HideFrom.d.ts
@@ -4,6 +4,7 @@ import { GrafanaItem } from "./GrafanaItem.js";
 import type { GenericMetaOptions } from "@gdacm/base-types";
 
 export declare class HideFrom extends GrafanaItem {
+    setGraph(graph: Boolean): this;
     setLegend(legend: Boolean): this;
     setTooltip(tooltip: Boolean): this;
     setViz(viz: Boolean): this;
