e6d1d8dcaa3725e6b6b522098402ba9b0064f75c
gissehel <public-dev-git-capella@gissehel.org>
2026-09-16 12:54:16 +0200

    CLEANUP : Remove unused comments

--- packages/grafana-items/src/items/PieChartLegendOptions.js
+++ packages/grafana-items/src/items/PieChartLegendOptions.js
@@ -4,10 +4,4 @@ import { VizLegendOptions } from "./VizLegendOptions.js";
 
 export const PieChartLegendOptions = defineGrafanaItemClass('PieChartLegendOptions', VizLegendOptions)
     .defineBasicArray('values', String)
-    // .defineValue('foo1', String)
-    // .defineValue('foo2', Number)
-    // .defineGrafanaObject('foo3', Foo, {
-    //     onDefault: (metaOptions) => getFoo(metaOptions),
-    // })
-    // .defineArray('foo4s', Bar)
     .asClass
